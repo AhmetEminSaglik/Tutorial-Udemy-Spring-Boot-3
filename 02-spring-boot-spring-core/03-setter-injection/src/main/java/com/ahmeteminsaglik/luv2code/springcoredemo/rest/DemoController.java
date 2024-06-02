6f1fa2148c734092e7888aa7bdeef7b19c45d56b
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-06-02 11:40:32 +0300

    setter injection is learned

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/ahmeteminsaglik/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package com.ahmeteminsaglik.luv2code.springcoredemo.rest;
+
+import com.ahmeteminsaglik.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    /*@Autowired
+    public DemoController(Coach theCoach) {
+        this.myCoach = theCoach;
+    }*/
+
+    @Autowired
+    public void setMyCoach(Coach theCoach) {
+//    public void doSomeStuff(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
