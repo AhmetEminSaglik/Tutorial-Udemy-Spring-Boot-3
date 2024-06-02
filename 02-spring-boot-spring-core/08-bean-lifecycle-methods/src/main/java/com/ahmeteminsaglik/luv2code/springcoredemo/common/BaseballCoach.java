20540d9feb4bac7904967f89237a95ce6d471f4d
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-06-02 16:24:53 +0300

    Bean lifecycle is learned(@PostContruct and @PreDestroy)

---
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/ahmeteminsaglik/luv2code/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,15 @@
+package com.ahmeteminsaglik.luv2code.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "For Baseball: Spend 30 minutes in batting practise";
+    }
+}
