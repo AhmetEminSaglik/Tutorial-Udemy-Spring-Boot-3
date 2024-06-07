7667755e8dedcbe03de49568fd4b64d08a61c568
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-06-07 16:35:48 +0300

    Update-ResetAuto-Increment Value SQL Command is added

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demmo-student/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demmo-student/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/dao/StudentDAO.java
@@ -4,4 +4,6 @@ import com.ahmeteminsaglik.luv2code.cruddemo.entity.Student;
 
 public interface StudentDAO {
     void save(Student student);
+    // to increate auto increment to 3000 :(SQL COMMAND)  ALTER TABLE student AUTO_INCREMENT =3000
+    // to reset auto increment to 1  :(SQL COMMAND) TRUNCATE student;
 }
