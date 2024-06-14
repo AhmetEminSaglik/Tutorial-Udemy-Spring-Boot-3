12b41e2245810ab57eb6fd332bb3a0fb528cc2ea
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-06-14 11:24:46 +0300

    @Service is added. Created EmployeeService and EmployeeServiceImpl

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/rest/EmployeeRestController.java
@@ -1,8 +1,7 @@
 package com.ahmeteminsaglik.luv2code.cruddemo.rest;
 
-import com.ahmeteminsaglik.luv2code.cruddemo.dao.EmployeeDAO;
 import com.ahmeteminsaglik.luv2code.cruddemo.entity.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ahmeteminsaglik.luv2code.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +12,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    //    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.ahmeteminsaglik.luv2code.cruddemo.service;
+
+import com.ahmeteminsaglik.luv2code.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+}
