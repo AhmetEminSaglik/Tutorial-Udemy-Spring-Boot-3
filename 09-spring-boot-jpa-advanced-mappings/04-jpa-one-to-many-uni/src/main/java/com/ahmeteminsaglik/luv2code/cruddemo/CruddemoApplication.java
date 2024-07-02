c9a59e30371da570895959ba7c10b6eb4d17b6ef
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-07-02 05:49:06 +0300

    Deleting Course and it's all reviews function is added.

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/ahmeteminsaglik/luv2code/cruddemo/CruddemoApplication.java
@@ -23,9 +23,18 @@ public class CruddemoApplication {
     public CommandLineRunner commandLineRunner(AppDAO appDAO) {
         return runner -> {
 //            createCourseAndReviews(appDAO);
-            retrieveCourseAndReviews(appDAO);
-
+//            retrieveCourseAndReviews(appDAO);
+            deleteCourseAndReviews(appDAO);
         };
+    }
+
+    private void deleteCourseAndReviews(AppDAO appDAO) {
+        int id = 10;
+        System.out.println("Delete Course id: " + id);
+        appDAO.deleteCourseById(id);
+        System.out.println("Done!");
+
+
     }
 
     private void retrieveCourseAndReviews(AppDAO appDAO) {
